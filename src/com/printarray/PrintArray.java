44337442e68a3af6e2b903c6f3d92fab84ffa2fd
dbasumatary <basudhondip@gmail.com>
2023-02-19 16:00:44 +0530

    Added: Array of different data types using generics

--- src/com/printarray/PrintArray.java
+++ src/com/printarray/PrintArray.java
@@ -1,28 +1,16 @@
 package com.printarray;
-/*Printing an array of integer, double and character*/
+/*Printing an array of integer, double and character using generics*/
 
 public class PrintArray {
-    public static void toPrint(Integer[] elements){             //array of integer
-        for (int element : elements) {
-            System.out.print(element + "  ");
-        }
-        System.out.println();
-    }
-    public static void toPrint(Double[] elements){               //array of double
-        for (double element : elements) {
-            System.out.print(element + "  ");
-        }
-        System.out.println();
-    }
-    public static void toPrint(Character[] elements){            //array of character
-        for (char element : elements) {
-            System.out.print(element + "  ");
+    public static <E> void toPrint(E[] elements){             //declare different variables that are generic types
+        for (E element : elements) {
+            System.out.printf("%s ", element);
         }
         System.out.println();
     }
     public static void main(String[] args) {
-        Integer[] integer = {1,2,3,4,5,6};
-        Double[] doubleNumber = {1.2,3.2,4.1,1.1,5.3,3.7};
+        Integer[] integer = {1, 2, 3, 4, 5, 6};
+        Double[] doubleNumber = {1.2, 3.2, 4.1, 1.1, 5.3, 3.7};
         Character[] character = {'e','q','k','s','v','d'};
         PrintArray.toPrint(integer);
         PrintArray.toPrint(doubleNumber);
